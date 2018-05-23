b5c12c7757be840edaf4f5235f31834ad24d80c5
girlmyra <girlmyra@163.com>
2018-05-23 03:37:14 +0000

    finsh static pages

---
+++ blog/resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sample')-Laravel beginner tutorial</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
